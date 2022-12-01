c93ce73a05543c35b7358bb4d5fd5f9a6c8ea30c
Anastasiya <9399023@mail.ru>
2022-12-01 13:09:45 +0300

    add tasks

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7, min = 5
+// a = 2 b = 10 -> max = 10, min = 2
+// a = -9 b = -3 -> max = -3, min = -9
+Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("max="+a);
+    Console.WriteLine("min="+b);
+}
+else
+{
+    Console.WriteLine("max="+b);
+    Console.WriteLine("min="+a);
+}
---
+++ task4/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход
+// три числа и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.WriteLine("Введите первое число: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c=Convert.ToInt32(Console.ReadLine());
+// int max=Math.Max(a, Math.Max(b,c));            это работает
+// Console.WriteLine(max);                        это работает     
+//прогуглила про Math.Max
+
+int max=a;
+if (b>a)
+{
+    max=b;
+}
+else 
+{
+    max=a;
+}
+if (c>max)
+{
+    max=c;
+    Console.WriteLine(max);
+}
+else
+{
+    Console.WriteLine(max);
+}
